ea856a06cdc4dda94fa08d4cbf419efb25789989
hzf <huangzefeng30@gmail.com>
2016-06-10 15:46:03 +0800

    数据操作类

--- app/src/main/java/com/commons/files/FileTest.java
+++ app/src/main/java/com/commons/files/FileTest.java
@@ -1,21 +1,34 @@
 package com.commons.files;
 
-import com.commons.FileUtils;
-import java.io.ByteArrayOutputStream;
+import com.commons.DataBuffer;
+import com.commons.DataReader;
 import java.io.File;
-import java.io.IOException;
 
 /**
  * Created by huangzefeng on 16/6/10.
  */
 public class FileTest {
 
-  public static void main(String[] args){
-
-    String path = "/Users/huangzefeng/Documents/test_txt.txt";
+  public static void main(String[] args) {
+    String path = "/Users/huangzefeng/Documents/test_data_stream";
     File txtFile = new File(path);
-    String content = FileUtils.readFileString(txtFile);
-    System.out.println("content="+content);
-  }
 
+    DataBuffer buffer = new DataBuffer();
+    buffer.writeInt(200);
+    buffer.writeChar('b');
+    buffer.writeBoolean(true);
+    System.out.println("size=" + buffer.size());
+    buffer.writeString("测试输入流");
+    buffer.writeToFile(txtFile);
+
+    DataReader reader = DataReader.createFromFile(txtFile);
+    int r1 = reader.readInt();
+    System.out.println("r1=" + r1);
+    char r2 = reader.readChar();
+    System.out.println("r2=" + r2);
+    boolean r3 = reader.readBoolean();
+    System.out.println("r3=" + r3);
+    String r5 = reader.readString();
+    System.out.print("r5=" + r5);
+  }
 }
---
+++ library/src/main/java/com/commons/DataReader.java
@@ -0,0 +1,90 @@
+package com.commons;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by huangzefeng on 16/6/10.
+ */
+public class DataReader {
+
+  private DataInputStream dis;
+
+  public static DataReader createFromFile(File file){
+    try {
+      return new DataReader(new FileInputStream(file));
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  public DataReader(InputStream is) {
+    dis = new DataInputStream(is);
+  }
+
+  public byte readByte() {
+    try {
+      if (dis != null) return dis.readByte();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return -1;
+  }
+
+  public int readInt() {
+    try {
+      if (dis != null) return dis.readInt();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return -1;
+  }
+
+  public char readChar(){
+    if(dis!=null) try {
+      return dis.readChar();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return 0;
+  }
+
+  public boolean readBoolean(){
+    if(dis!=null) try {
+      return dis.readBoolean();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return false;
+  }
+
+  public float readFloat(){
+    if(dis!=null) try {
+      return dis.readFloat();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return 0;
+  }
+
+  public String readString(){
+    if(dis!=null) try {
+      return dis.readUTF();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return null;
+  }
+
+}
